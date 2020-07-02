6f258f8e78a2bc7dd2766a7b08d69c244bbe119d
johnventura <ventura@engination.com>
2020-07-01 23:45:32 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,296 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// FileDiff is the structure of an individual patch
+type FileDiff struct {
+	// Start is an offset inside a file where data is changed
+	Start int `json:"start"`
+	// Offset is added to Start to define where to modify data
+	// Sometimes, Offset is necessary when you are near the
+	// end of a block of code and you are using shasum
+	Offset int `json:"offset"`
+	// NewBytes is the new data being inserted
+	NewBytes string `json:"newbytes"`
+	// Shasum is the SHA256 hash of a block of binary data
+	Shasum string `json:"shasum"`
+	// Comment should include a description of the change
+	Comment string `json:"comment"`
+}
+
+// PatchFile allows for multiple differences to be describe in one file
+type PatchFile struct {
+	// Patch includes an array of FileDiff file changes
+	Patch []FileDiff `json:"patch"`
+}
+
+// ReadFileToMemory reads a file into memory and returns it as a []byte
+func ReadFileToMemory(fileName string) ([]byte, error) {
+	fileBuf, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return fileBuf, nil
+
+}
+
+// WriteBytesToDisk writes a chunk of memory to a file
+func WriteBytesToDisk(fileName string, fileBuf []byte) error {
+	err := ioutil.WriteFile(fileName, fileBuf, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetOffsetFromHash returns an offset of a block of data that has a specified sha256 hash
+func GetOffsetFromHash(memBuf []byte, inHash []byte) (int, error) {
+	parseLimit := len(memBuf) - sha256.BlockSize
+	for i := 0; i < parseLimit; i++ {
+		shaSum := sha256.Sum256(memBuf[i : i+sha256.BlockSize])
+		if bytes.Equal(inHash[:], shaSum[:]) {
+			return i, nil
+		}
+	}
+	// we couldn't find the block with the given hash
+	return 0, errors.New("Data not found")
+}
+
+// GetHashFromOffset returns a sha256 hash for a block of data in a []byte array
+func GetHashFromOffset(memBuf []byte, offset int) ([32]byte, error) {
+	if (offset + sha256.BlockSize) > len(memBuf) {
+		return [32]byte{}, errors.New("Out of bounds for ROM")
+	}
+	shaSum := sha256.Sum256(memBuf[offset : offset+sha256.BlockSize])
+	return shaSum, nil
+}
+
+// FindDifference compares two byte arrays and returns difference as []FileDiff array
+func FindDifference(bufSrc []byte, bufAlt []byte) []FileDiff {
+	diffStart := 0
+	diffStartOld := -1
+	rDiffs := []FileDiff{}
+	// compare each byte.
+	for i := 0; i < len(bufSrc); i++ {
+		// if the bytes don't match at a given offset, figure out where
+		// the difference occured
+		if (bufSrc[i] != bufAlt[i]) && diffStart != diffStartOld {
+			diffStart = i
+			diffStartOld = i
+		}
+		// calculate the SHA of the block where the change started
+		if (bufSrc[i] == bufAlt[i]) && diffStart == diffStartOld {
+			diffEnd := i
+			newDiff := FileDiff{}
+			newDiff.Start = diffStart
+			newDiff.Offset = 0
+			newDiff.NewBytes = hex.EncodeToString(bufAlt[diffStart:diffEnd])
+			rDiffs = append(rDiffs, newDiff)
+			diffStartOld = -1
+		}
+	}
+	return rDiffs
+}
+
+// PatchByteArray reads a parsed Patchfile, and returns byte array copy with patches applied
+func PatchByteArray(bufSrc []byte, patchFile PatchFile) ([]byte, error) {
+	newBuf := make([]byte, len(bufSrc))
+	copy(newBuf, bufSrc)
+	for _, patch := range patchFile.Patch {
+		newBytes, err := hex.DecodeString(patch.NewBytes)
+		if err != nil {
+			return newBuf, err
+		}
+		if patch.Shasum == "" {
+			copy(newBuf[patch.Start:(patch.Start+len(newBytes))], newBytes)
+
+		} else {
+			for i := 0; i <= len(bufSrc)-sha256.BlockSize; i++ {
+				newHash, err := GetHashFromOffset(bufSrc, i)
+				if err != nil {
+					return newBuf, err
+				}
+				displayHash := hex.EncodeToString(newHash[:])
+				if displayHash == patch.Shasum {
+					patchLocation := i + patch.Offset
+					copy(newBuf[patchLocation:(patchLocation+len(newBytes))], newBytes)
+				}
+			}
+		}
+		if patch.Comment != "" {
+			fmt.Printf("%v\n", patch.Comment)
+		}
+
+	}
+	return newBuf, nil
+}
+
+// ReadPatchJson parses a Json formatted patch file and returns a struct
+func ReadPatchJson(inJson string) (PatchFile, error) {
+
+	var patchFile PatchFile
+	err := json.Unmarshal([]byte(inJson), &patchFile)
+	if err != nil {
+		return patchFile, err
+	}
+
+	return patchFile, nil
+}
+
+// cmdDiff reads two filenames and returns a Json formatted diff
+func cmdDiff(fileNameSrc string, fileNameAlt string, OutputFormat int8) (string, error) {
+	fileBuf, err := ReadFileToMemory(fileNameSrc)
+	if err != nil {
+		return "", err
+	}
+
+	fileBufAlt, err := ReadFileToMemory(fileNameAlt)
+	if err != nil {
+		return "", err
+	}
+
+	diffStr, err := DiffsToJson(fileBuf, fileBufAlt, OutputFormat)
+	if err != nil {
+		return "", err
+	}
+	return diffStr, nil
+}
+
+// cmdPatch apply binary patches and return patched buffer
+func cmdPatch(fileNameSrc string, patchFileName string) ([]byte, error) {
+	rBytes := []byte{}
+
+	patchFileBuf, err := ReadFileToMemory(patchFileName)
+	if err != nil {
+		return rBytes, err
+	}
+	originalFileBuf, err := ReadFileToMemory(fileNameSrc)
+	if err != nil {
+		return rBytes, err
+	}
+
+	patchFile, err := ReadPatchJson(string(patchFileBuf))
+	if err != nil {
+		return rBytes, err
+	}
+
+	rBytes, err = PatchByteArray(originalFileBuf, patchFile)
+	if err != nil {
+		return rBytes, err
+	}
+
+	return rBytes, nil
+}
+
+// these constants describe what patch data should be included
+var (
+	DiffsWithSHA    int8 = 1
+	DiffsWithOffset int8 = 2
+)
+
+// DiffsToJson compares two byte arrays and describes the difference as Json
+func DiffsToJson(fileBuf []byte, fileBufAlt []byte, OutputType int8) (string, error) {
+	rStr := ""
+	fileDiffs := FindDifference(fileBuf, fileBufAlt)
+	rStr = fmt.Sprintf(`{"patch": [`)
+	for i, fileDiff := range fileDiffs {
+		displayHash := ""
+		if fileDiff.Start > (len(fileBuf) - sha256.BlockSize) {
+			fileDiff.Offset = fileDiff.Start - (len(fileBuf) - sha256.BlockSize)
+			fileDiff.Start = (len(fileBuf) - sha256.BlockSize)
+		}
+
+		// suppress SHA, if user wants to
+		if (OutputType & DiffsWithSHA) != 0 {
+			newHash, err := GetHashFromOffset(fileBuf, fileDiff.Start)
+			if err != nil {
+				return "", err
+			}
+			displayHash = hex.EncodeToString(newHash[:])
+		}
+		newBytes := fileDiff.NewBytes
+		if i > 0 {
+			rStr = rStr + fmt.Sprintf(",")
+		}
+		// suppress offset, if user wants to
+		if (OutputType & DiffsWithOffset) == 0 {
+			fileDiff.Start = 0
+		}
+		rStr = rStr + fmt.Sprintf(`{"start": %v, "offset": %v, "newbytes": "%v", "shasum": "%v", "comment": ""}`, fileDiff.Start, fileDiff.Offset, newBytes, displayHash)
+	}
+	rStr = rStr + fmt.Sprintf("]}\n")
+	return rStr, nil
+}
+
+// usage helps the user by displaying usage information
+func usage(progName string) {
+	fmt.Printf("%v is a utility for patching ROMs.\nUsage:\n", progName)
+	fmt.Printf("%v [command] filenames...\n", progName)
+	fmt.Printf("%v diff [oringial file] [modified file]\n", progName)
+	fmt.Printf("\t diff creates a Json formatted description of differences between the files\n")
+	fmt.Printf("%v patch [original file] [Json patch file] [output file name]\n", progName)
+	fmt.Printf("\t patch reads a Json formatted patch file and creates a modfied copy\n")
+	os.Exit(1)
+}
+
+func main() {
+	ArgC := len(os.Args)
+	// user type command with no paramaters
+	if ArgC <= 2 {
+		usage(os.Args[0])
+	}
+	command := os.Args[1]
+
+	switch command {
+	// user wants to compare two files
+	case "diff":
+		if ArgC <= 3 {
+			usage(os.Args[0])
+		}
+		// optional commands for patch file output
+		diffMode := DiffsWithSHA | DiffsWithOffset
+		if ArgC > 4 {
+			switch os.Args[4] {
+			case "nosha":
+				diffMode = diffMode ^ DiffsWithSHA
+			case "nooffset":
+				diffMode = diffMode ^ DiffsWithOffset
+			}
+		}
+		OriginalFileName := os.Args[2]
+		ModFileName := os.Args[3]
+		jsonDiffOut, err := cmdDiff(OriginalFileName, ModFileName, diffMode)
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		}
+		fmt.Printf("%s\n", jsonDiffOut)
+	// user wants to apply a Json formatted patch
+	case "patch":
+		OriginalFileName := os.Args[2]
+		diffFileName := os.Args[3]
+		outBuf, err := cmdPatch(OriginalFileName, diffFileName)
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		}
+		outFileName := OriginalFileName + ".patched.bin"
+		if ArgC <= 3 {
+			outFileName = os.Args[4]
+		}
+
+		err = WriteBytesToDisk(outFileName, outBuf)
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+		}
+	default:
+		usage(os.Args[0])
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestDiffsToJson(t *testing.T) {
+	originalStr := []byte(
+		`The second Conference at Basra, though many of its prominent figures had already played leading rôles in the earlier gathering in 1965, was very different in scale, scope and spirit from that assembly. It was an older gathering. The average age, says Amen Rihani, was a full ten years higher. Young men were still coming into the Fellowship abundantly, but there had also been accessions—and not always very helpful accessions—of older men who had been radical and revolutionary leaders in the war period. Their frame of experience had shaped them for irresponsible resistance. Their mental disposition was often obstructively critical and insubordinate. Many had had no sort of technical experience. They were disposed to throw an anarchistic flavour over schools and propaganda.
+
+Moreover, the great scheme of the Modern State had now lost something of its first compelling freshness. The "young men of '65" had had ten years of responsible administrative work. They had been in contact with urgent detail for most of that period. They had had to modify De Windt's generalizations in many particulars, and the large splendour of the whole project no longer had the same dominating power over their minds. They had lost something of the professional esprit de corps, the close intimate confidence with each other, with which they had originally embarked upon the great adventure of the Modern State. Many had married women of the older social tradition and formed new systems of gratification and friendship. They had ceased to be enthusiastic young men and they had become men of the world. The consequent loss of a sure touch upon primary issues was particularly evident in the opening sessions.`)
+
+	altStr := []byte(
+		`The second Conference at Basra, though many of its prominent figures had already played leading rôles in the earlier gathering in 1965, was very different in scale, scope and spirit from that assembly. It was an older gathering. The average age, says Amen Rihani, was a full ten years higher. Young men were still coming into the Fellowship abundantly, but there had also been accessions—and not always very hurtful accessions—of older men who had been radical and revolutionary leaders in the war period. Their frame of experience had shaped them for irresponsible resistance. Their mental disposition was often obstructively critical and insubordinate. Many had had no sort of technical experience. They were disposed to throw an anarchistic flavour over schools and propaganda.
+
+Moreover, the great scheme of the Modern State had now lost something of its first compelling freshness. The "young men of '65" had had ten years of responsible administrative work. They had been in contact with urgent detail for most of that period. They had had to modify De Windt's generalizations in many particulars, and the large splendour of the whole project no longer had the same dominating power over their minds. They had lost something of the professional esprit de corps, the close intimate confidence with each other, with which they had originally embarked upon the great adventure of the Modern State. Many had married women of the older social tradition and formed new systems of gratification and friendship. They had ceased to be enthusiastic young men and they had become men of the world. The consequent loss of a sure touch upon primary issues was particularly evident in the closing sessions.`)
+
+	resultWeWant := `{"patch": [{"start": 412, "offset": 0, "newbytes": "757274", "shasum": "843c295955090eb4b25b449f8b03b9ab88027d83ca888209eab597fc5149a67c", "comment": ""},{"start": 1639, "offset": 47, "newbytes": "636c6f73", "shasum": "d7ab7c3249fca6159a6d786d7e9a09de83684378efca68ebc1374e01fbd499d5", "comment": ""}]}` + "\n"
+
+	reportStr, err := DiffsToJson(originalStr, altStr, int8(3))
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	assert.Equal(t, reportStr, resultWeWant, "Patch file generation worked")
+}
+
+func TestPatchByteArray(t *testing.T) {
+
+	TestPatchFile := `
+{"patch": [{"start": 940, "offset": 0, "newbytes": "706c6179", "shasum": "", "comment": "Chunk 1 changed"},{"start": 949, "offset": 0, "newbytes": "73", "shasum": "d5c4ad8394b778304665f8593d95a86ef144a5d40b0a0111b5099d11cad864e6", "comment": ""},{"start": 951, "end": 961, "offset": 0, "newbytes": "41746172692032363030", "shasum": "4d8277b2ab827426a81d80adebb954ea54b635ef9b2dbe8871ffc752787ea249", "comment": ""}]}`
+
+	TextOriginal :=
+		`In a castle of Westphalia, belonging to the Baron of
+Thunder-ten-Tronckh, lived a youth, whom nature had endowed with the
+most gentle manners. His countenance was a true picture of his soul. He
+combined a true judgment with simplicity of spirit, which was the
+reason, I apprehend, of his being called Candide. The old servants of
+the family suspected him to have been the son of the Baron's sister, by
+a good, honest gentleman of the neighborhood, whom that young lady would
+never marry because he had been able to prove only seventy-one
+quarterings, the rest of his genealogical tree having been lost through
+the injuries of time.
+
+The Baron was one of the most powerful lords in Westphalia, for his
+castle had not only a gate, but windows. His great hall, even, was hung
+with tapestry. All the dogs of his farm-yards formed a pack of hounds at
+need; his grooms were his huntsmen; and the curate of the village was
+his grand almoner. They called him "My Lord," and laughed at all his
+stories.
+
+The Baron's lady weighed about three hundred and fifty pounds, and was
+therefore a person of great consideration, and she did the honours of
+the house with a dignity that commanded still greater respect. Her
+daughter Cunegonde was seventeen years of age, fresh-coloured, comely,
+plump, and desirable. The Baron's son seemed to be in every respect
+worthy of his father. The Preceptor Pangloss[1] was the oracle of the
+family, and little Candide heard his lessons with all the good faith of
+his age and character.
+
+Pangloss was professor of metaphysico-theologico-cosmolo-nigology. He
+proved admirably that there is no effect without a cause, and that, in
+this best of all possible worlds, the Baron's castle was the most
+magnificent of castles, and his lady the best of all possible
+Baronesses.
+`
+	TextWeWant :=
+		`In a castle of Westphalia, belonging to the Baron of
+Thunder-ten-Tronckh, lived a youth, whom nature had endowed with the
+most gentle manners. His countenance was a true picture of his soul. He
+combined a true judgment with simplicity of spirit, which was the
+reason, I apprehend, of his being called Candide. The old servants of
+the family suspected him to have been the son of the Baron's sister, by
+a good, honest gentleman of the neighborhood, whom that young lady would
+never marry because he had been able to prove only seventy-one
+quarterings, the rest of his genealogical tree having been lost through
+the injuries of time.
+
+The Baron was one of the most powerful lords in Westphalia, for his
+castle had not only a gate, but windows. His great hall, even, was hung
+with tapestry. All the dogs of his farm-yards formed a pack of hounds at
+need; his grooms were his huntsmen; and the curate of the village was
+his grand almoner. They played his Atari 2600 and laughed at all his
+stories.
+
+The Baron's lady weighed about three hundred and fifty pounds, and was
+therefore a person of great consideration, and she did the honours of
+the house with a dignity that commanded still greater respect. Her
+daughter Cunegonde was seventeen years of age, fresh-coloured, comely,
+plump, and desirable. The Baron's son seemed to be in every respect
+worthy of his father. The Preceptor Pangloss[1] was the oracle of the
+family, and little Candide heard his lessons with all the good faith of
+his age and character.
+
+Pangloss was professor of metaphysico-theologico-cosmolo-nigology. He
+proved admirably that there is no effect without a cause, and that, in
+this best of all possible worlds, the Baron's castle was the most
+magnificent of castles, and his lady the best of all possible
+Baronesses.
+`
+	patchJson, err := ReadPatchJson(TestPatchFile)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	testAlt, err := PatchByteArray([]byte(TextOriginal), patchJson)
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		os.Exit(1)
+	}
+	assert.Equal(t, string(testAlt), TextWeWant, "Patch output worked")
+}
